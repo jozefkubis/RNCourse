4276e4d1eb41c947694bcc58c85a4bf45495aa03
jozef kubis <kubis.jozef@outlook.com>
2026-02-12 19:56:26 +0100

    Implement input handling for course goals and display goals list

--- App.js
+++ App.js
@@ -1,14 +1,39 @@
+import { useState } from "react"
 import { Button, StyleSheet, Text, TextInput, View } from "react-native"
 
 export default function App() {
+  const [enteredGoalText, setEnteredGoalText] = useState("")
+  const [courseGoals, setCourseGoals] = useState([])
+
+  function goalIntputHandler(enteredText) {
+    setEnteredGoalText(enteredText)
+  }
+
+  function addGoalHandler() {
+    setCourseGoals((currentCourseGoals) => [
+      ...currentCourseGoals,
+      enteredGoalText,
+    ])
+    setEnteredGoalText("")
+  }
+
   return (
     <View style={styles.appContainer}>
       <View style={styles.inputContainer}>
-        <TextInput style={styles.textInput} placeholder="Your course goal!" />
-        <Button title="Add Goal" />
+        <TextInput
+          style={styles.textInput}
+          placeholder="Your course goal!"
+          onChangeText={goalIntputHandler}
+          value={enteredGoalText}
+        />
+        <Button title="Add Goal" onPress={addGoalHandler} />
       </View>
-      <View>
-        <Text>List of goals...</Text>
+      <View style={styles.goalsContainer}>
+        {courseGoals.map((goal, i) => (
+          <View key={i}>
+            <Text>{goal}</Text>
+          </View>
+        ))}
       </View>
     </View>
   )
@@ -16,17 +41,27 @@ export default function App() {
 
 const styles = StyleSheet.create({
   appContainer: {
+    flex: 1,
     padding: 50,
+    paddingHorizontal: 16,
   },
   inputContainer: {
+    flex: 1,
     flexDirection: "row",
     justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: "#cccccc",
   },
   textInput: {
     borderWidth: 1,
     borderColor: "#cccccc",
-    width: "80%",
+    width: "70%",
     marginRight: 8,
     padding: 8,
+  },
+  goalsContainer: {
+    flex: 5,
   },
 })
