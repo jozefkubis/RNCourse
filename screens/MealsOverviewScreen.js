b057fc167a1b7b9899633282fed9a3f9e0082506
jozef kubis <kubis.jozef@outlook.com>
2026-05-19 08:59:40 +0200

    Add MealsOverviewScreen and implement navigation from CategoriesScreen

---
+++ screens/MealsOverviewScreen.js
@@ -0,0 +1,18 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function MealsOverviewScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>Meals Overview Screen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
