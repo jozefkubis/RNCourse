5253e34c6a781238624257a4a7f0479b4960cabd
jozef kubis <kubis.jozef@outlook.com>
2026-05-20 19:21:55 +0200

    Refactor MealItem component to accept additional props for enhanced meal details display

--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -10,7 +10,15 @@ export default function MealsOverviewScreen({ route }) {
   });
 
   function renderMealItem(itemData) {
-    return <MealItem title={itemData.item.title} />;
+    const mealItemProps = {
+      title: itemData.item.title,
+      imageUrl: itemData.item.imageUrl,
+      duration: itemData.item.duration,
+      complexity: itemData.item.complexity,
+      affordability: itemData.item.affordability,
+    };
+
+    return <MealItem {...mealItemProps} />;
   }
 
   return (
