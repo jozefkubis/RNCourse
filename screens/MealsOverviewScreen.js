f499009a7476b3cc92ff54580afbc1ac34762b9e
Jozef Kubis <kubis.jozef@outlook.com>
2026-05-23 21:46:48 +0200

    feat: implement favorites functionality with context provider and update related screens

--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,6 +1,5 @@
 import { useLayoutEffect } from "react";
-import { FlatList, StyleSheet, View } from "react-native";
-import MealItem from "../components/MealItem";
+import MealsList from "../components/MealsList/MealsList";
 import { CATEGORIES, MEALS } from "../data/dummy-data";
 
 export default function MealsOverviewScreen({ route, navigation }) {
@@ -20,33 +19,5 @@ export default function MealsOverviewScreen({ route, navigation }) {
     });
   }, [categoryTitle, navigation]);
 
-  function renderMealItem(itemData) {
-    const mealItemProps = {
-      id: itemData.item.id,
-      title: itemData.item.title,
-      imageUrl: itemData.item.imageUrl,
-      duration: itemData.item.duration,
-      complexity: itemData.item.complexity,
-      affordability: itemData.item.affordability,
-    };
-
-    return <MealItem {...mealItemProps} />;
-  }
-
-  return (
-    <View style={styles.container}>
-      <FlatList
-        data={dispalyedMeals}
-        keyExtractor={(item) => item.id}
-        renderItem={renderMealItem}
-      />
-    </View>
-  );
+  return <MealsList items={dispalyedMeals} />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 16,
-  },
-});
